53bdb3502caa9a2f17b187fb7eae2a18c45b971e
Angel Granado <angel.granado@correo.unimet.edu.ve>
2023-02-03 22:57:03 -0400

    Clase Queue lista, falta hacer debug :white_check_mark:

--- Proyecto1_EDD/src/MainClasses/Queue.java
+++ Proyecto1_EDD/src/MainClasses/Queue.java
@@ -20,7 +20,7 @@ public class Queue<T> {
     private Nodo<T> rear;
     
     /**
-     * Constructor
+     * Constructor de la clase.
      */
     
     public Queue(){
@@ -29,7 +29,62 @@ public class Queue<T> {
     }
     
     /**
-     * Retorna el nodo que le precede
+     * Getter para acceder a length.
+     * 
+     * @return length el tamaño de la cola.
+     */
+    public int getLength(){
+        return length;
+    }
+    
+    /**
+     * Setter para modificarl a length.
+     * 
+     * @param newLength nuevo tamaño de la cola.
+     */
+    public void setLegth(int newLength){
+        this.length = newLength;
+    }
+    
+    /**
+     * Getter para acceder al primer nodo en cola.
+     * 
+     * @return front primer nodo en la cola.
+     */
+    public Nodo<T> getFront(){
+        return front;
+    }
+    
+    /**
+     * Setter para modificar el primer nodo en cola.
+     * 
+     * @param newFront el nuevo nodo que estara al inicio de la cola.
+     */
+    public void setFrond(Nodo<T> newFront){
+        this.front = newFront;
+    }
+    
+    /**
+     * Getter para acceder al ultimo nodo en cola.
+     * 
+     * @return rear el ultimo nodo en cola.
+     */
+    public Nodo<T> getRear(){
+        return rear;
+    }
+    
+    /**
+     * Setter para modificar el ultimo nodo en cola.
+     * 
+     * @param newRear nuevo nodo que sera el ultimo de la cola.
+     */
+    public void setRear(Nodo<T> newRear){
+        this.rear = newRear;
+    }
+    
+    /**
+     * Retorna el nodo que le precede.
+     * 
      * @param nodo nodo dado
      * @return nextNodo 
      */
@@ -75,7 +130,7 @@ public class Queue<T> {
         T aux;
         if (!isEmpty()){
             aux = front.gettInfo();
-            front = next(front);
+            this.front = next(front);
             length--;
         } else throw new Exception("No hay elementos en cola.");
         return aux;
@@ -86,7 +141,7 @@ public class Queue<T> {
      */
     public void destroyQueue(){
         for(; front != null;){
-            front = next(front);
+            this.front = next(front);
         }
         System.gc();
     }
